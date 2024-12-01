a0176e1a4c4fe93270e73a9f630ce23567b86e2f
Andrey <andrey.kozhevnikov@toptal.com>
2024-12-01 16:21:22 -0500

    1 day done

--- src/components/Compass.vue
+++ src/components/Compass.vue
@@ -52,7 +52,7 @@
         {
             flag: RS,
             day: 1,
-            done: false,
+            done: true,
             welcomeUrl:   'https://drive.google.com/file/d/1zBCjg7Fz0jRN2Q_bVeFxPyZOqigdQxzK/view',
             endOfTaskUrl: 'https://drive.google.com/file/d/1RwnrXUjjE5rEwnhaNr_KFTiRcFvT5ppb/view',
             symbolUrl: 'https://iili.io/208jbI9.png'
